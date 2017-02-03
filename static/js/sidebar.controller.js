14c1bd42617e2b615e33cdd188464c8e87d4b1c1
HackAfro <richyafro@gmail.com>
2017-02-03 16:58:51 +0100

    animate

--- static/js/sidebar.controller.js
+++ static/js/sidebar.controller.js
@@ -1,56 +1,56 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	var office = angular.module('naijalogosOffice');
+    var office = angular.module('naijalogosOffice');
 
-	office.controller('sideBarCtrl', ['$scope','$localForage','$timeout','$location', function($scope,$localForage,$timeout,$location){
+    office.controller('sideBarCtrl', ['$scope', '$localForage', '$timeout', '$location', function ($scope, $localForage, $timeout, $location) {
 
         function get() {
             $localForage.getItem('user').then(function (data) {
                 $scope.user = data
             })
 
-            $timeout(get,5000)
+            $timeout(get, 5000)
         }
-        
-	    $timeout(get,5000)
-        
-		$scope.page = 0
 
-        
+        $timeout(get, 5000)
+
+        $scope.page = 0
+
+
         $scope.activePage = function (page) {
             return $scope.page === page
         }
-        
-        $scope.imprests = function(page) {
-        	$scope.page = page
-			$location.url('/imprests')
-			$('.ui.left.sidebar').sidebar('hide')
-		}
-        
-        $scope.vendors = function(page) {
-        	$scope.page = page
-			$location.url('/vendorforms')
-			$('.ui.left.sidebar').sidebar('hide')
-		}
-        
-        $scope.billboards = function(page) {
-        	$scope.page = page
-			$location.url('/vendorforms')
-			$('.ui.left.sidebar').sidebar('hide')
-		}
-        
-        $scope.accounts = function(page) {
-        	$scope.page = page
-			$location.url('/account')
-			$('.ui.left.sidebar').sidebar('hide')
-		}
-        
-        $scope.jobs = function(page) {
-        	$scope.page = page
-			$location.url('/jobs')
-			$('.ui.left.sidebar').sidebar('hide')
-		}
 
-	}])
+        $scope.imprests = function (page) {
+            $scope.page = page
+            $location.url('/imprests')
+            $('.ui.left.sidebar').sidebar('hide')
+        }
+
+        $scope.vendors = function (page) {
+            $scope.page = page
+            $location.url('/vendorforms')
+            $('.ui.left.sidebar').sidebar('hide')
+        }
+
+        $scope.billboards = function (page) {
+            $scope.page = page
+            $location.url('/vendorforms')
+            $('.ui.left.sidebar').sidebar('hide')
+        }
+
+        $scope.accounts = function (page) {
+            $scope.page = page
+            $location.url('/account')
+            $('.ui.left.sidebar').sidebar('hide')
+        }
+
+        $scope.jobs = function (page) {
+            $scope.page = page
+            $location.url('/jobs')
+            $('.ui.left.sidebar').sidebar('hide')
+        }
+
+    }])
 })()
